ff45e41b4671fa91e48865e53a3e160a82ff0731
Guillermo Hernandez <guillermo.hernandez@correounivalle.edu.co>
2018-10-24 16:46:45 -0500

    Application of the differents clicks options

--- app/src/main/java/com/example/guillermoandres/myappusingkotlin/Activities/ClickEventeActivity.kt
+++ app/src/main/java/com/example/guillermoandres/myappusingkotlin/Activities/ClickEventeActivity.kt
@@ -2,13 +2,53 @@ package com.example.guillermoandres.myappusingkotlin.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.guillermoandres.myappusingkotlin.R
 
 
-class ClickEventeActivity : AppCompatActivity() {
+class ClickEventeActivity : AppCompatActivity(), View.OnLongClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_evente)
+
+        //Captura de botones
+        val button1 = findViewById<Button>(R.id.buttonClickMulti1)
+        val button2 = findViewById<Button>(R.id.buttonClickMulti2)
+        val button3 = findViewById<Button>(R.id.buttonClickMulti3)
+
+        button1.setOnLongClickListener(this)
+        button2.setOnLongClickListener(this)
+        button3.setOnLongClickListener(this)
+        clickInLine()
     }
+
+    // 1. Click en XML
+    // 2. Click en en linea
+    // 1. Click en por ID´s
+
+    //Reglas obligatorias para clicks en XML
+    //El método debes ser público. Los nombres tienen que coincidir. Recibe un solo parametro
+    fun xmlClick(view: View){
+        Toast.makeText(this, "Clickeado por XML", Toast.LENGTH_LONG).show()
+    }
+
+    //En linea
+    private fun clickInLine(){
+        val btn = findViewById<Button>(R.id.buttonClickInLine)
+        btn.setOnClickListener{Toast.makeText(this, "Clickeado en Linea", Toast.LENGTH_LONG).show()}
+    }
+
+    override fun onLongClick(v: View): Boolean {
+        when(v.id){
+            R.id.buttonClickMulti1 -> Toast.makeText(this, "Clickeado Multi1", Toast.LENGTH_LONG).show()
+            R.id.buttonClickMulti2 -> Toast.makeText(this, "Clickeado Multi2", Toast.LENGTH_LONG).show()
+            R.id.buttonClickMulti3 -> Toast.makeText(this, "Clickeado Multi3", Toast.LENGTH_LONG).show()
+        }
+        return true
+    }
+
+
 }
