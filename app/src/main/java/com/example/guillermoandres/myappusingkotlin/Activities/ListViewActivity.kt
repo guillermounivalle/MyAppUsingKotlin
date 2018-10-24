7354f20b5367abfde4ebfdc59ed677388e95451b
Guillermo Hernandez <guillermo.hernandez@correounivalle.edu.co>
2018-10-24 15:07:31 -0500

    Using intent basic

---
+++ app/src/main/java/com/example/guillermoandres/myappusingkotlin/Activities/ListViewActivity.kt
@@ -0,0 +1,13 @@
+package com.example.guillermoandres.myappusingkotlin.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.guillermoandres.myappusingkotlin.R
+
+class ListViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
